837b6b81afa27d99cc90fbaf2060a174a5892cc1
Rohan <72726137+rohan2266@users.noreply.github.com>
2022-11-10 00:40:48 +0530

    Update Screen.java

--- src/sortvisualiser/screens/Screen.java
+++ src/sortvisualiser/screens/Screen.java
@@ -10,16 +10,16 @@ import javax.swing.JPanel;
 import sortvisualiser.MainApp;
 
 public abstract class Screen extends JPanel {
-    protected MainApp app;
+    protected MainApp app;   // object of MainApp class
 
     public Screen(MainApp app) {
-        this.app = app;
+        this.app = app;       // setter function
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(WIN_WIDTH, WIN_HEIGHT);
+        return new Dimension(WIN_WIDTH, WIN_HEIGHT);    // return dimension of window
     }
 
-    public abstract void onOpen();
+    public abstract void onOpen(); //abstract onOpen method used for starting a sort ting window from beginning
 }
